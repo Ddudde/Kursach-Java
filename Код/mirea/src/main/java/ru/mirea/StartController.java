3c112987f3a1050149bc8728a6205dfb5506f5ef
Dude <BAX.152MM@yandex.ru>
2021-06-15 21:26:57 +0300

    v#1

--- Код/mirea/src/main/java/ru/mirea/StartController.java
+++ Код/mirea/src/main/java/ru/mirea/StartController.java
@@ -2,11 +2,33 @@ package ru.mirea;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.ScrollPane;
+import javafx.scene.input.KeyCode;
+import javafx.scene.layout.Pane;
+
+import java.awt.*;
+import java.awt.event.KeyEvent;
 
 public class StartController {
 
     @FXML
     private ScrollPane scrpan;
+
+    @FXML
+    private Pane auth;
+
+    public void initialize()
+    {
+        //caps();
+        auth.setOnKeyReleased(event -> {
+            if (event.getCode() == KeyCode.CAPS) caps();
+        });
+    }
+
+    public void caps()
+    {
+        boolean isCapsLockOn = Toolkit.getDefaultToolkit().getLockingKeyState(KeyEvent.VK_CAPS_LOCK);
+        System.out.println(isCapsLockOn);
+    }
 
     public void down()
     {
